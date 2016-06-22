879994c2b16ee47c8fb6a8e8336b83edeca6e34c
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2016-06-22 11:14:59 -0500

    Made basic game layout for simple simon

---
+++ public/index.html/index.js
@@ -0,0 +1,20 @@
+'use strict';
+// 			 var box = $('.box');
+
+// 			 box.animate({
+// 				 'width': '75px',
+// 				 'height': '100px'
+// 			 }, 3000, function() {
+// 				 console.log("Done.");
+// 			 }).animate ({
+// 				 'width': '20px'
+// 			 }, 2000, function() {
+// 				 console.log('Done again.');
+// 			 });
+// 			 setTimeout(function() {
+// 				 console.log("Hello Kings");
+// 			 }, 3000)
+
+function newRound() {
+	var colorSequence = Math.floor(Math.random()* 3) + 1;
+}
